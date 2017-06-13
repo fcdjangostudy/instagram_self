755e99fd0bb49cb5a3554f2159f62f97f761b3a0
GAIUS8 <gaius827@gmail.com>
2017-06-14 00:17:05 +0900

    Create post_detail funtions

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 
 User = get_user_model()
@@ -11,3 +11,11 @@ def post_list(request):
         'posts': posts
     }
     return render(request, 'post/post_list.html', context)
+
+
+def post_detail(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'post/post_detail.html', context)
