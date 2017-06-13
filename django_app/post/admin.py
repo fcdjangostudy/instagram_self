55878f879e629b66465410895684a8eaea0f549d
GAIUS8 <gaius827@gmail.com>
2017-06-13 20:30:48 +0900

    Create admin page

--- django_app/post/admin.py
+++ django_app/post/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, PostLike, Comment, CommentLike
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(PostLike)
+admin.site.register(Comment)
+admin.site.register(CommentLike)
