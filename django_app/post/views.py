6fd29b2bfbd31ad8ec349f1aef28df95c31be093
GAIUS8 <gaius827@gmail.com>
2017-06-14 00:37:18 +0900

    Create post_create functions

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -19,3 +19,23 @@ def post_detail(request, post_pk):
         'post': post
     }
     return render(request, 'post/post_detail.html', context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        user = User.objects.first()
+        post = Post.objects.create(
+            author=user,
+            photo=request.FILES['file']  # 'file'은 POST 요청시 input[type="file"]이 가진 name 속성
+        )
+        comment_string = request.POST.get('comment', '')
+        if comment_string:
+            post.comment_set.create(
+                author=user,
+                content=comment_string,
+            )
+        else:
+            pass
+        return redirect('post:post_detail', post_pk=post.pk)
+    else:
+        return render(request, 'post/post_create.html')
