90c48bfe83e8c93432ac7bf9b955cc8c9fd80203
GAIUS8 <gaius827@gmail.com>
2017-06-15 11:45:52 +0900

    Create login functions and make template for login

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('post:post_list')
+        else:
+            return HttpResponse('login Error')
+    else:
+        return render(request, 'member/login.html')
