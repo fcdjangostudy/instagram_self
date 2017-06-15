106b746eb7da859f1e562423e60a6064e10d0f00
GAIUS8 <gaius827@gmail.com>
2017-06-15 12:24:56 +0900

    Create login functions

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as django_login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -7,11 +7,19 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
         if user is not None:
-            login(request, user)
+            django_login(request, user)
             return redirect('post:post_list')
         else:
             return HttpResponse('login Error')
     else:
+        # 만약 이미 로그인 된 상태일 경우에는 post_list로 redirect
+        if request.user.is_authenticated:
+            return redirect('post:post_list')
         return render(request, 'member/login.html')
+
